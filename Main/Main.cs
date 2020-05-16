aa0800f321c430ec24cb0b56e36ebf6cdf64b345
QingShanKing <qingshanking@vip.qq.com>
2020-05-16 14:31:42 +0800

    更新接口数据，变更域名；优化域名解析错误报错

--- Main/Main.cs
+++ Main/Main.cs
@@ -27,6 +27,11 @@ namespace Main
             {
                 //int doc_id = Convert.ToInt32(doc_id_str);
                 var ressstr = HttpRequest(doc_id_str);
+                if (ressstr.Contains("未能解析此远程名称"))
+                {
+                    MessageBox.Show("网络异常");
+                    return;
+                }
                 ResModel hm = JsonConvert.DeserializeObject<ResModel>(ressstr);
                 //数据获取成功
                 if (hm.code == 0)
@@ -48,15 +53,7 @@ namespace Main
             {//不是INT
                 long_text.Text += "\n文章id错误";
             }
-
-
-
-
-        }
-        private void d_btn_Click(object sender, EventArgs e)
-        {
-
-        }
+        }        
 
 
         /// <summary>
@@ -66,28 +63,36 @@ namespace Main
         /// <returns></returns>
         private string HttpRequest(string doc_id)
         {
-            string url = "https://v2.album.bilibili.xiaose.llili.top/getdetail.php";
-            HttpHelper http = new HttpHelper();
-            HttpItem item = new HttpItem()
+            try
             {
-                //URL     必需项  
-                URL = url + "?doc_id=" + doc_id,
-                Method = "GET",//URL     可选项 默认为Get  
-                Timeout = 100000,//连接超时时间     可选项默认为100000  
-                ReadWriteTimeout = 30000,//写入Post数据超时时间     可选项默认为30000  
-                IsToLower = false,//得到的HTML代码是否转成小写     可选项默认转小写  
-                Cookie = "",//字符串Cookie     可选项  
-                UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0",//用户的浏览器类型，版本，操作系统     可选项有默认值  
-                Accept = "text/html, application/xhtml+xml, */*",//    可选项有默认值  
-                ContentType = "text/html",//返回类型    可选项有默认值  
-                Referer = "",//来源URL     
-                Postdata = "",
-                ResultType = ResultType.String,//返回数据类型，是Byte还是String  
-            };
-            HttpResult result = http.GetHtml(item);
-            string html = result.Html;
-            string cookie = result.Cookie;
-            return html;
+                string url = "http://h-bilibili.xs.170601.xyz/v2/getdetail.php";
+                HttpHelper http = new HttpHelper();
+                HttpItem item = new HttpItem()
+                {
+                    //URL     必需项  
+                    URL = url + "?doc_id=" + doc_id,
+                    Method = "GET",//URL     可选项 默认为Get  
+                    Timeout = 100000,//连接超时时间     可选项默认为100000  
+                    ReadWriteTimeout = 30000,//写入Post数据超时时间     可选项默认为30000  
+                    IsToLower = false,//得到的HTML代码是否转成小写     可选项默认转小写  
+                    Cookie = "",//字符串Cookie     可选项  
+                    UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0",//用户的浏览器类型，版本，操作系统     可选项有默认值  
+                    Accept = "text/html, application/xhtml+xml, */*",//    可选项有默认值  
+                    ContentType = "text/html",//返回类型    可选项有默认值  
+                    Referer = "",//来源URL     
+                    Postdata = "",
+                    ResultType = ResultType.String,//返回数据类型，是Byte还是String  
+                };
+                HttpResult result = http.GetHtml(item);
+                string html = result.Html;
+                string cookie = result.Cookie;
+                return html;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+
         }
         /// <summary>
         /// 关于按钮
---
+++ packages/CSkin.16.1.14.3/lib/README.md
@@ -0,0 +1,2 @@
+# cskin
+CSKIN备份文件 不错的WinForm第三方控件
